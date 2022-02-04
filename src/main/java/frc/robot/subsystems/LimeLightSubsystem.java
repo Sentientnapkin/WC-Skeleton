22c127bbc8b23b6f63033d128c0d4b2e37d655b4
JTkeio <jacobtkeio@gmail.com>
2022-02-03 22:49:25 -0800

    Add features in order to test TurnToGoal and getting values from the limelight

--- src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -1,28 +1,38 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import static edu.wpi.first.wpilibj.smartdashboard.SmartDashboard.putNumber;
+import frc.robot.commands.auto.GoalNotFoundException;
 
 // See http://docs.limelightvision.io/en/latest/networktables_api.html
 
+
 public class LimeLightSubsystem extends SubsystemBase {
     final NetworkTable limelight;
+    double[] limelightOutputArray;
+    double[] defaultLimelightOutputArray = {-1, -1, -1, -1, -1, -1, -1, -1};
 
     public LimeLightSubsystem(String tableName) {
         this.limelight = NetworkTableInstance.getDefault().getTable(tableName);
+        limelightOutputArray = defaultLimelightOutputArray;
+    }
+
+    public double getLimelightOutputAtIndex(int index) throws GoalNotFoundException {
+        if (index>8||index<0) {
+            throw new IndexOutOfBoundsException();
+        } else if (limelightOutputArray!=defaultLimelightOutputArray) {
+            return limelightOutputArray[index];
+        } else {
+            throw new GoalNotFoundException();
+        }
     }
 
     public void periodic() {
-        double x = limelight.getEntry("tlong").getDouble(0.0);
-        putNumber("tlong", x);
-        putNumber("distance in cm", 40/Math.tan(59.6*x/(2*960)));
-    }
-
-    public NetworkTable grabNetworkTable() {
-        return this.limelight;
+        limelightOutputArray = limelight.getEntry("llpython").getDoubleArray(defaultLimelightOutputArray);
+        System.out.println(limelightOutputArray[1]);
+        SmartDashboard.putNumber("xOffset", limelightOutputArray[1]);
     }
 }
+
